f49a5e9b6364f1f8af3cb8f75476f8cefe8ee5bd
UnknownBallin <50780247+UnknownBallin@users.noreply.github.com>
2019-07-10 09:43:58 -0600

    Update PingTagTask.php

--- src/UnknownBallin/PingTag/PingTagTask.php
+++ src/UnknownBallin/PingTag/PingTagTask.php
@@ -2,40 +2,15 @@
 
 namespace UnknownBallin\PingTag;
 
-use pocketmine\Server;
-
 use pocketmine\scheduler\Task;
 
 class PingTagTask extends Task{
-
-		public function onRun(int $tick):void{
-
-		foreach(Server::getInstance()->getOnlinePlayers() as $player){
-
-			$playerping=$player->getPing();
-
-			if($playerping >= 0){
-
-				$ping="§a".$playerping."ms";
-
-				}
-
-				if($playerping >= 50){
-
-					$ping="§e".$playerping."ms";
-
-					}
-
-					if($playerping >= 100){
-
-						$ping="§c".$playerping."ms";
-
-						}
-
-						$player->setScoreTag($ping);
-
+	
+	public function onRun(int $tick):void{
+		foreach(PingTag::getInstance()->getServer()->getOnlinePlayers() as $player){
+			$tag=PingTag::getInstance()->getConfig()->get("Format");
+			$tag=str_replace(array("{ping}"), $player->getPing(), $tag);
+			$player->setScoreTag($tag);
 		}
-
 	}
-
 }
