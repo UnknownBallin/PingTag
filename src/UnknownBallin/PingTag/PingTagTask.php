02a5e7dc46a335383f02ecbf57628132341cf22c
UnknownBallin <50780247+UnknownBallin@users.noreply.github.com>
2019-07-10 11:20:26 -0600

    Oops

--- src/UnknownBallin/PingTag/PingTagTask.php
+++ src/UnknownBallin/PingTag/PingTagTask.php
@@ -9,7 +9,7 @@ class PingTagTask extends Task{
 	public function onRun(int $tick):void{
 		foreach(PingTag::getInstance()->getServer()->getOnlinePlayers() as $player){
 			$tag=PingTag::getInstance()->getConfig()->get("Format");
-			$tag=str_replace(array("{ping}"), $player->getPing(), $tag);
+			$tag=str_replace("{ping}", $player->getPing(), $tag);
 			$player->setScoreTag($tag);
 		}
 	}
